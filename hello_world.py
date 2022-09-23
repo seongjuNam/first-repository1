237a20fc57c4b937883ac6485f789a304c6564f7
seongjuNam <nsj6319@gmail.com>
2022-09-23 16:08:20 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
-def print_hello():
-    print("Hello World!!")
-
-if __name__ == '__main__':
-    print_hello()
+def add(x, y):
+a = x + y
+return a
+def print_KIT():
+print("KIT")
+if __name__ == '__main__’:
+print_KIT()
+print( print_hello(1,2) )
